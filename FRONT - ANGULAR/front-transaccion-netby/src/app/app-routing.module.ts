07e766b226e44e355cf3487efe646ebddb62ea55
Gabriel NUñez V. <38358137+Makotobka@users.noreply.github.com>
2026-08-29 20:42:43 -0500

    Eliminacion de componente redundante

--- FRONT - ANGULAR/front-transaccion-netby/src/app/app-routing.module.ts
+++ FRONT - ANGULAR/front-transaccion-netby/src/app/app-routing.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { TransaccionesComponent } from './Template/transacciones/transacciones.component';
-import { InicioComponent } from './Template/inicio/inicio.component';
-import { ProductosComponent } from './Template/productos/productos.component';
+import { TransaccionesComponent } from './View/transacciones/transacciones.component';
+import { ProductosComponent } from './View/productos/productos.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'inicio', pathMatch: 'full' }, //Defaul URL
-  { path: '**', redirectTo: 'inicio' }, //Defaul URL
+  // { path: '', redirectTo: 'inicio', pathMatch: 'full' }, //Defaul URL
+  // { path: '**', redirectTo: 'inicio' }, //Defaul URL
 
   { path: 'transacciones', component: TransaccionesComponent },
   { path: 'productos', component: ProductosComponent },
-  { path: 'inicio', component: InicioComponent },
 ];
 
 @NgModule({
--- FRONT - ANGULAR/front-transaccion-netby/src/app/app.module.ts
+++ FRONT - ANGULAR/front-transaccion-netby/src/app/app.module.ts
@@ -4,23 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { InicioComponent } from './Template/inicio/inicio.component';
-import { ProductosComponent } from './Template/productos/productos.component';
-import { TransaccionesComponent } from './Template/transacciones/transacciones.component';
+import { ProductosComponent } from './View/productos/productos.component';
+import { TransaccionesComponent } from './View/transacciones/transacciones.component';
+import { TemplateModule } from './Template/Template.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    InicioComponent,
-    ProductosComponent,
-    TransaccionesComponent
-  ],
+  declarations: [AppComponent, ProductosComponent, TransaccionesComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    TemplateModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
